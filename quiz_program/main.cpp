4234209c73ee727cdf0e838b616d0505682c2ee2
shawn yu <shawn.yu5@hotmail.com>
2021-08-25 18:25:11 -0400

    quiz_program/main.cpp updated

--- quiz_program/main.cpp
+++ quiz_program/main.cpp
@@ -12,83 +12,92 @@ int main () {
 
     Quiz quiz;
 
-    quiz.addQuestion(Question{"Which of the following is an example of a fundamental data type?",
-            "function",
-            "char",
-            "array",
-            "class",
-            2
-            });
-
-    quiz.addQuestion(Question{"Which of the following is an example of a fundamental data type?",
-            "function",
+    quiz.addQuestion(Question{"Which data type is used to create a variable that should store text?",
+            std::vector<string>
+            {
             "string",
-            "array",
-            "class",
-            2});
-
-    quiz.addQuestion(Question{"Which type of conversion is known as 'Automatic Conversion'?",
-            "Explicit Conversion",
-            "Auto Conversion",
-            "Implicit Conversion",
-            "Simple Conversion",
-            3});
-
-    quiz.addQuestion(Question{"Symbols that perform operations on variables are called...?",
-            "Operators",
-            "Data Types",
-            "Variables",
-            "Modifiers",
+            "Txt",
+            "String",
+            "myString",
+            },
             1});
 
-    quiz.addQuestion(Question{"Which of the following is NOT an example of an operator?",
-            "Logical Operators",
-            "Arithmetic Operators",
-            "Relational Operators",
-            "Symbol Operators",
-            4});
-
-    quiz.addQuestion(Question{"Which symbol expresses 'not equal'?",
-            ">=",
-            "!=",
-            "<=",
-            "==",
+    quiz.addQuestion(Question{"How do you create a variable with the numeric value 5?",
+            std::vector<string> {
+            "double x = 5;",
+            "int x = 5;",
+            "x = 5;",
+            "num x = 5;",
+            "string x = 5;"
+            },
             2});
 
-    quiz.addQuestion(Question{"Which is the correct format for single line comment?",
-            "/*Comments",
-            "##Comments",
-            "//Comments",
-            "**Comments",
-            3});
-
-    quiz.addQuestion(Question{"Which of the following expresses a logical 'OR'?",
-            "||",
-            "&&",
-            "!",
-            "OR",
-            1});
-
-    quiz.addQuestion(Question{"Which of the following is the symbol for remainder after division?",
-            "-",
-            "=",
-            "^",
-            "%",
-            4});
-
-
-    quiz.addQuestion(Question{"The increment operator, '++', increases the value of the operand by...?",
-            "7",
-            "2",
-            "1",
+    quiz.addQuestion(Question{"Array indexes start at index:",
+            std::vector<string>
+            {
             "0",
+            "1",
+            },
+            1});
+
+    quiz.addQuestion(Question
+            {"Which method can be used to find the length of a string?",
+            std::vector<string>
+            {
+            "len()",
+            "length()",
+            "getSize()",
+            "getLength()"
+            },
+            2});
+
+    quiz.addQuestion(Question{"Which header file lets us work with input and output objects?",
+            std::vector<string>
+            {
+            "#include <stream>",
+            "#include <iosstream>",
+            "#include <inputstream>",
+            "#include <iostream>"
+            },
+            4});
+
+    quiz.addQuestion(Question{"Which operator can be used to compare two values?",
+            std::vector<string>
+            {
+            "<>",
+            "=",
+            "==",
+            "><"
+            },
             3});
 
     quiz.addQuestion(Question{"Which is the correct format for the cout statement?",
-            "'cout'",
-            "cout <<",
-            "cout >>",
-            "<cout>",
+            std::vector<string>
+            {
+            "'cout' << \"hello world\";",
+            "cout << \"hello world\";",
+            "cout >> \"hello world\";",
+            "<cout> \"hello world\";"
+            },
+            2});
+
+    quiz.addQuestion(Question{"What is the difference between a class and struct in C++?",
+            std::vector<string>
+            {
+            "they named different",
+            "Classes allow private members, while structs does not",
+            "Structs allow private members, while classes does not",
+            "there is no difference"
+            },
+            2});
+
+    quiz.addQuestion(Question{"What is a vector?",
+            std::vector<string>
+            {
+            "An array that requires the user to manage memory",
+            "an array that automatically grows and shrinks as it needs",
+            "None of the above"
+            },
             2});
 
     quiz.run(quiz);
