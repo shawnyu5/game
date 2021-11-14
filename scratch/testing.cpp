7914248048647d6d50989225650362b58bc75e29
Shawn Yu <shawn.yu5@hotmail.com>
2021-11-13 22:46:05 -0500

    repo reoriginiztion

---
+++ scratch/testing.cpp
@@ -0,0 +1,19 @@
+//purpose of this file:
+//Date: 2021-07-17
+//---------------------------------
+#include <iostream>
+#include <string>
+#include <iomanip>
+using std::cout;
+using std::endl;
+using std::string;
+
+int main ()
+{
+    cout << "program excuting" << '\n';
+    int input;
+    std::cin >> input;
+    cout << input;
+}
+
+
